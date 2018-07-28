d1cef66ca51c558cc820a266b64e5a83dd3e1a7a
League <League@AdminisorsiMac2.attlocal.net>
2018-07-27 18:27:44 -0700

    Backpack,HarryRunner

---
+++ src/HarryRunner.java
@@ -0,0 +1,23 @@
+
+public class HarryRunner {
+public static void main(String[] args) {
+	HarryPotter harrypotter = new HarryPotter();
+	harrypotter.makeInvisible(true);
+	harrypotter.spyOnSnape();
+	harrypotter.makeInvisible(false);
+	harrypotter.castSpell("stupefy");
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
