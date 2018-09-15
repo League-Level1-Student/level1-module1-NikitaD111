1ba793f0b0975bf131ef46a61715fc4558324932
NikitaD111 <league@WTS-IM-17.attlocal.net>
2018-09-14 18:28:23 -0700

    BinaryConverter(ip)

--- src/BinaryConverter.java
+++ src/BinaryConverter.java
@@ -1,3 +1,6 @@
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -5,25 +8,29 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class BinaryConverter {
+public class BinaryConverter implements MouseListener  {
 public static void main(String[] args) {
-	JFrame frame = new JFrame();
-	JPanel panel = new JPanel();
-	JTextField answer = new JTextField(20);
-	JButton button = new JButton();
-	JLabel label = new JLabel();
-	frame.setVisible(true);
-	frame.setSize(512, 112);
-	frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
-	button.setText("convert");
-	frame.add(panel);
-	panel.add(answer);
-	panel.add(label);
-	panel.add(button);
-	
-	frame.pack();
+	new BinaryConverter().binary();
 	
 }
+void binary() {
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton button = new JButton();
+	JLabel label = new JLabel();
+	JTextField answer = new JTextField(20);
+	frame.setVisible(true);
+	frame.setSize(612, 112);
+	button.setText("convert");
+ 	button.addMouseListener(this);
+	panel.add(answer);
+	panel.add(button);
+	panel.add(label);
+	frame.add(panel);
+	frame.pack();
+	frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+
+ }
 String convert(String input) {
           if(input.length() != 8){
                JOptionPane.showMessageDialog(null, "Enter 8 bits, silly!!!");
@@ -43,6 +50,31 @@ String convert(String input) {
                return "-";
           }
      }
+@Override
+public void mouseClicked(MouseEvent e) {
+	// TODO Auto-generated method stub
+
+}
+@Override
+public void mousePressed(MouseEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+@Override
+public void mouseReleased(MouseEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+@Override
+public void mouseEntered(MouseEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+@Override
+public void mouseExited(MouseEvent e) {
+	// TODO Auto-generated method stub
+	
+}
 
 
 
